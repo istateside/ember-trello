f0ded6978a0face7836865c975e493e9cd7983e3
istateside <kevin.r.fleischman@gmail.com>
2016-05-08 20:31:01 -0400

    adds fixme statement, removes other routes

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,6 +1,4 @@
 export default function() {
   this.get('/lists')
   this.get('/lists/:id', ['list', 'cards'])
-  this.get('/cards')
-  this.get('/cards/:id')
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,6 @@ export default Ember.Route.extend({
   model() {
     const lists =  this.store.findAll('list');
     const mappedLists = lists.map(function(list) { return { list: list, cards: list.get('cards') }})
-    return { lists: mappedLists }
+    return { lists: mappedLists } // FIXME: Not working.
   }
 });
